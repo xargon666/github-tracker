85dca8f2533015fc897b4fcdf5143df183d038b1
Amir <aha000111@icloud.com>
2022-06-22 10:09:34 +0100

    updated App function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { useState } from "react";
 import './App.css'
 
-function App () {
+function App() {
+    const [username, setUsername] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        searchRepos();
+    }
+    
+
+
+
+
+
     return (
         <div className="page">
             <div className="landing-page-container">
